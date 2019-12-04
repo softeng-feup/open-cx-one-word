ffb53fd0001f908c8155d97f17c8de99ac577894
Hedwig87 <freitas.henrique1999@gmail.com>
2019-12-04 23:28:15 +0000

    trying to get all posts working

--- my_conference/lib/pages/feedPage.dart
+++ my_conference/lib/pages/feedPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_conference/services/authService.dart';
+import 'package:my_conference/services/databaseService.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class FeedPage extends StatefulWidget {
 
@@ -10,8 +13,76 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
+
+  Future<List<DocumentSnapshot>> _getPosts() async {
+    QuerySnapshot query = await DatabaseService.getAllPosts();
+    return query.documents;
+  }
+
+  Widget buildPosts(BuildContext context, AsyncSnapshot snapshot) {
+    List<DocumentSnapshot> posts = snapshot.data;
+    Future<List<String>> names = DatabaseService.getNameOfUserFromPosts(posts);
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return new ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: (BuildContext context, int index) {
+        Future<String> name = DatabaseService.getNameOfUserId(posts[index]['authorId']);
+
+        return Container (
+          color: Color(0xFFEBE1E1),
+          child: Row(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      child: CachedNetworkImage(
+                        height: width / 3,
+                        width: width / 3,
+                        fit: BoxFit.contain,
+                        imageUrl: posts[index]['imgUrl'],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: 3* width / 10,
+                      ),
+                      child: Container(
+                        height: 3 * width / 10,
+                        margin: EdgeInsets.only(right: 5),
+                        child: SingleChildScrollView(
+                          child: Text(posts[index]['description']),
+                        ),
+                      )
+                  ),
+                  Container(
+                    child: Text(names[index]),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var userPosts = ;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF8C2D19),
@@ -25,16 +96,7 @@ class _FeedPageState extends State<FeedPage> {
           ),
         )
       ),
-      body: Container(
-        color: Color(0xFFEBE1E1),
-        child: Center(
-          child: FlatButton(
-            onPressed: () => AuthService.logout(),
-            color: Colors.blue,
-            child: Text('Logout'),
-          ),
-        ),
-      ),
+      body: userPosts
     );
   }
 }
--- my_conference/lib/services/databaseService.dart
+++ my_conference/lib/services/databaseService.dart
@@ -18,13 +18,27 @@ class DatabaseService{
     return snapshots;
   }
 
+  static Future<QuerySnapshot> getAllPosts() {
+    return postsRef.orderBy('timestamp', descending: true).getDocuments();
+  }
+
   static Future<DocumentSnapshot> getUserDoc(String uid) async {
     return usersRef.document(uid).get();
   }
 
-  static Future<String> getNameOfUserId(String uid) async {
+  static  Future<String> getNameOfUserId(String uid) async {
     DocumentSnapshot doc = await getUserDoc(uid);
     return doc.data['name'].toString();
+  }
+
+  static Future<List<String>> getNameOfUserFromPosts(List<DocumentSnapshot> posts) async {
+    List<String> names = [];
+
+    for(int i = 0; i < posts.length; i++) {
+      String name = await getNameOfUserId(posts[i]['authorId']);
+      names.add( name);
+    }
+    return names;
   }
 
   //TODO
